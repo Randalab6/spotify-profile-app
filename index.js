b26b636383236db5b8a74b6af2fccf988df72039
randalab6 <randalab6@gmail.com>
2023-04-25 14:20:16 -0500

    remove extra queryString const

--- index.js
+++ index.js
@@ -8,7 +8,6 @@ const port = 8888;
 const CLIENT_ID = process.env.CLIENT_ID;
 const CLIENT_SECRET = process.env.CLIENT_SECRET;
 const REDIRECT_URI = process.env.REDIRECT_URI;
-const querystring = require('querystring');
 
 //route handler 
 app.get('/', (req, res) => {
