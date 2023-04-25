fd02d0b594e781327d51cb9d729316980f437e4d
randalab6 <randalab6@gmail.com>
2023-04-25 11:44:47 -0500

    add redirectURL

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 require('dotenv').config()
-console.log(process.env) // remove this after you've confirmed it is working
 const express = require('express');
 const app = express();
 const port = 8888;
