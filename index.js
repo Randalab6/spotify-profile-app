df3de2a8a25eae30dd4f76b2baddca31e917bb31
randalab6 <randalab6@gmail.com>
2023-04-25 11:54:25 -0500

    refactor query param

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const port = 8888;
 const CLIENT_ID = process.env.CLIENT_ID;
 const CLIENT_SECRET = process.env.CLIENT_SECRET;
 const REDIRECT_URI = process.env.REDIRECT_URI;
+const querystring = require('querystring');
 
 //route handler 
 app.get('/', (req, res) => {
@@ -16,10 +17,16 @@ app.get('/', (req, res) => {
     res.json(data)
   });
 
-  app.get('/login', (req, res) => {
-    res.redirect(`https://accounts.spotify.com/authorize?client_id=${CLIENT_ID}&response_type=code&redirect_uri=${REDIRECT_URI}`);
+app.get('/login', (req, res) => {
+  const queryParams = querystring.stringify({
+    client_id: CLIENT_ID,
+    response_type: 'code',
+    redirect_uri: REDIRECT_URI,
   });
-  
+
+  res.redirect(`https://accounts.spotify.com/authorize?${queryParams}`);
+});
+
 //listen for connection
 app.listen(port, () => {
     console.log(`Express app listening at http://localhost:${port}`);
