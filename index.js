5a2976266d8aface98e4a29a7dbbcc4f07d12545
randalab6 <randalab6@gmail.com>
2023-04-28 17:32:24 -0500

    modify scope to fix 403 error

--- index.js
+++ index.js
@@ -32,14 +32,17 @@ const generateRandomString = length => {
   return text;
 };
 
-
 const stateKey = 'spotify_auth_state';
 
 app.get('/login', (req, res) => {
   const state = generateRandomString(16);
   res.cookie(stateKey, state);
 
-  const scope = 'user-read-private user-read-email';
+  const scope = [
+    'user-read-private',
+    'user-read-email',
+    'user-top-read',
+  ].join(' ');
 
   const queryParams = querystring.stringify({
     client_id: CLIENT_ID,
