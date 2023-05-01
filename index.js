be741ee35ff2beeb755641a2e148605dc554158e
randalab6 <randalab6@gmail.com>
2023-04-30 19:25:49 -0500

    make it better

--- index.js
+++ index.js
@@ -3,11 +3,16 @@ const express = require('express');
 const querystring = require('querystring'); //to parse & stringfy query string
 const app = express();
 const axios = require('axios')
-const port = 8888;
+const path = require('path');
 
 const CLIENT_ID = process.env.CLIENT_ID;
 const CLIENT_SECRET = process.env.CLIENT_SECRET;
 const REDIRECT_URI = process.env.REDIRECT_URI;
+const FRONTEND_URI = process.env.FRONTEND_URI;
+const PORT = process.env.PORT || 8888;
+
+// Priority serve any static files.
+app.use(express.static(path.resolve(__dirname, './client/build')));
 
 //route handler 
 app.get('/', (req, res) => {
@@ -82,7 +87,7 @@ app.get('/callback', (req, res) => {
       });
 
      //redirect to react app
-      res.redirect(`http://localhost:3000/?${queryParams}`);
+      res.redirect(`${FRONTEND_URI}?${queryParams}`);
      //pass along tokens in quart params
      //if there's an error, redirect with error query params
     } else {
@@ -94,7 +99,11 @@ app.get('/callback', (req, res) => {
   });
 });
 
-//listen for connection
-app.listen(port, () => {
-    console.log(`Express app listening at http://localhost:${port}`);
+// All remaining requests return the React app, so it can handle routing. catch all
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, './client/build', 'index.html'));
+});
+
+app.listen(PORT, () => {
+  console.log(`Express app listening at http://localhost:${PORT}`);
 });
